dc1da30aaa8c60213d784188ee83fa8138aa99a7
Ashwin S. Nair <58840757+Ashwin-exe@users.noreply.github.com>
2022-06-22 11:33:55 +0530

    flask server to keep bot alive

---
+++ keep_alive.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from threading import Thread
+app=Flask("")
+@app.route('/')
+def home():
+  return "Hello.I'm alive!"
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keepalive():
+  server=Thread(target=run)
+  server.start()
