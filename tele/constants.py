0e3cf06171db82635fd14633ff0d776cdcc5a70a
Ashwin S. Nair <58840757+Ashwin-exe@users.noreply.github.com>
2022-07-22 10:19:50 +0530

    hide keys

--- tele/constants.py
+++ tele/constants.py
@@ -1,14 +1,11 @@
 import pytz
 
 
-API_KEY = '5460223513:AAFLCOuUnTAcX175NtPphOxss0Ghi7acYqo'
-# bot = telebot.TeleBot(API_KEY)
-CANVAS_URL = 'https://aheadonline.amrita.edu'
-CANVAS_TOKEN = 'yxDlbHuvnLUvX28wlbhtK9pu018QCz6pE2o0rrpXfWZG8tI2iyUfzp1B4SztRGr7'
+API_KEY = 'Your Telegram API KEY'
+CANVAS_URL = 'Your CANVAS DOMAIN'
+CANVAS_TOKEN = 'Your CANVAS TOKEN ID'
 IST = pytz.timezone('Asia/Kolkata')
 CANVAS_DATE_FORMAT = r'%Y-%m-%dT%H:%M:%SZ'
 OUTPUT_DATE_FORMAT = r'%a, %b %d at %I:%M %p'
-# API_KEY = '5460223513:AAFLCOuUnTAcX175NtPphOxss0Ghi7acYqo'  # telegram api
-
 # date and time format guide:
 # https://www.ibm.com/docs/en/workload-automation/9.5.0?topic=troubleshooting-date-time-format-reference-strftime
